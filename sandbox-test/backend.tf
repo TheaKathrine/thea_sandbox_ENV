67692cc00d77490d808164393e8f06cb18d1ee3a
TheaKathrine <thea.kathrine.baatrup.kampmann@regionh.dk>
2023-02-21 12:17:06 +0100

    unique state-file

--- sandbox-test/backend.tf
+++ sandbox-test/backend.tf
@@ -3,7 +3,7 @@ terraform {
     backend "azurerm" {
         storage_account_name = "tfbackendcimtsandboxstg"
         container_name       = "tfstatelock"
-        key                  = "GitEnvTest.tfstate"
+        key                  = "sandboxEnv.tfstate"
     }
 
 }
